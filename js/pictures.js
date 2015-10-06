5b5ab4baff83126666bbaa880806ad2c51e3ff00
vafelman <vafelman@gmail.com>
2015-10-07 00:02:23 +0300

    Add picture load and failure handler

--- js/pictures.js
+++ js/pictures.js
@@ -2,6 +2,8 @@
   var filters = document.querySelector('.filters');
   filters.classList.add('hidden');
 
+  var IMAGE_FAILURE_TIMEOUT = 10000;
+
   var picturesContainer = document.querySelector('.pictures');
   var pictureTemplate = document.getElementById('picture-template');
   var picturesFragment = document.createDocumentFragment();
@@ -11,9 +13,29 @@
 
     newPictureElement.querySelector('.picture-likes').textContent = picture['llikes'];
     newPictureElement.querySelector('.picture-comments').textContent = picture['comments'];
-    newPictureElement.querySelector('img').setAttribute('src', picture['url']);
 
     picturesFragment.appendChild(newPictureElement);
+
+    if (picture['url']) {
+      var photo = new Image();
+      photo.src = picture['url'];
+
+      var imageLoadTimeout = setTimeout(function() {
+        newPictureElement.classList.add('picture-load-failure');
+      }, IMAGE_FAILURE_TIMEOUT);
+
+      photo.onload = function() {
+        newPictureElement.querySelector('img').setAttribute('src', picture['url']);
+        newPictureElement.querySelector('img').width = 182;
+        newPictureElement.querySelector('img').height = 182;
+        clearTimeout(imageLoadTimeout);
+      }
+
+      photo.onerror = function(evt) {
+        newPictureElement.classList.add('picture-load-failure');
+      };
+    };
+
     picturesContainer.appendChild(picturesFragment);
   });
 })();
